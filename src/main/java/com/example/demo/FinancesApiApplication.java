45cf5f3d77e5943ea5c5dbec16df4568bbfb4552
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-24 20:45:37 -0300

    first commit

---
+++ src/main/java/com/example/demo/FinancesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesApiApplication.class, args);
+	}
+
+}
