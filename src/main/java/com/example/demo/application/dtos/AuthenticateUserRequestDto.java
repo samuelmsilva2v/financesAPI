a58a3f01cc0d8ad617418649d207e62711a5d0d0
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-28 15:02:50 -0300

    user dtos validations

--- src/main/java/com/example/demo/application/dtos/AuthenticateUserRequestDto.java
+++ src/main/java/com/example/demo/application/dtos/AuthenticateUserRequestDto.java
@@ -1,10 +1,18 @@
 package com.example.demo.application.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AuthenticateUserRequestDto {
 
+	@Email(message = "Please enter a valid email adress.")
+	@NotEmpty(message = "Please enter your access email.")
 	private String email;
+	
+	@Size(min = 8, message = "Please enter your login password with at least 8 characters")
+	@NotEmpty(message = "Please enter your login password.")
 	private String senha;
 }
--- src/main/java/com/example/demo/application/dtos/CreateUserRequestDto.java
+++ src/main/java/com/example/demo/application/dtos/CreateUserRequestDto.java
@@ -1,11 +1,23 @@
 package com.example.demo.application.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserRequestDto {
 
+	@Size(min = 4, max = 100, message = "Please enter a name between 4 and 100 characters.")
+	@NotEmpty(message = "Please enter your name.")
 	private String name;
+
+	@Email(message = "Please enter a valid email adress.")
+	@NotEmpty(message = "Please enter your access email.")
 	private String email;
+
+	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!])(?!.*\\s).{8,}$", message = "Please enter a password containing lowercase letters, uppercase letters, numbers, symbols, and at least 8 characters.")
+	@NotEmpty(message = "Please enter your login password.")
 	private String password;
 }
